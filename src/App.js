3743a39eca604051153ab8d549ee1c7fbc66ac8c
Sandro Santos <sandro.dev@gmail.com>
2020-03-30 19:02:12 -0300

    reactotron-react-js library and configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import { Router } from 'react-router-dom';
-import history from './services/history';
+
+import './config/ReactotronConfig';
+
 import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (

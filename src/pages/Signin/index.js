e1cea3221b2818597e0df201302af88fe20d29e6
Sandro Santos <sandro.dev@gmail.com>
2020-04-02 19:44:41 -0300

    Signin Page

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
 export default function Signin() {
-  return <h1>Signin</h1>;
+  return (
+    <>
+      <form>
+        <img src={logo} alt="logo" />
+        <label htmlFor="email">SEU E-MAIL:</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="exemplo@email.com"
+        />
+
+        <label htmlFor="password">SUA SENHA:</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="******"
+        />
+
+        <input type="submit" value="Entrar no sistema" />
+      </form>
+    </>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,6 +1,56 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
   background: #7d40e7;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  background: #fff;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 30px;
+  border-radius: 4px;
+  box-shadow: 0px 0px 10px ${darken(0.2, '#7d40e7')};
+  width: 360px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    img {
+      margin: 20px 0 30px;
+      width: 85%;
+    }
+
+    label {
+      color: #444;
+      font-weight: bold;
+    }
+
+    input {
+      height: 44px;
+      align-self: stretch;
+      padding: 0 15px;
+      margin: 10px 0 10px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #999;
+    }
+
+    input[type='submit'] {
+      height: 44px;
+      background: #7d40e7;
+      color: #fff;
+      font-weight: bold;
+      border-radius: 4px;
+      padding: 0 15px;
+      margin: 5px 0;
+    }
+  }
 `;

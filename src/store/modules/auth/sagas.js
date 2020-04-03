b7c330a273121287b4cc7789b71e2f609d083e10
Sandro Santos <sandro.dev@gmail.com>
2020-04-03 17:16:56 -0300

    action, reducer and saga for module auth

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,27 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess, signInFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', { email, password });
+
+    const { token, user } = response.data;
+
+    yield put(signInSuccess(token, user));
+
+    history.push('dashboard');
+
+    console.tron.log('Saga SIGNIN', response.data);
+  } catch (error) {
+    yield put(signInFailure());
+    console.tron.error('error saga module auth', error);
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);

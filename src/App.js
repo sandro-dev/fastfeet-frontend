f46c588f6ef4e87566dcd8f52d121ad969bce5e0
Sandro Santos <sandro.dev@gmail.com>
2020-04-04 20:45:02 -0300

    persisted reducer to persist data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
-import GlobalStyle from './styles/global';
 
+import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 import history from './services/history';
-import store from './store';
+
+import { store, persistor } from './store';
+
+import GlobalStyle from '~/styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }

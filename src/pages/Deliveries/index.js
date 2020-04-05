6d28658d2db44b16e95da69aa19d27ff75534b9e
Sandro Santos <sandro.dev@gmail.com>
2020-04-05 13:22:45 -0300

    deliveries page

---
+++ src/pages/Deliveries/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Dashboard() {
+  return (
+    <>
+      <h1>Gerenciando encomendas</h1>
+      <input name="search" placeholder="Buscar por encomendas" />
+    </>
+  );
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,13 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import Signin from '~/pages/Signin';
-import Dashboard from '~/pages/Dashboard';
+import Deliveries from '~/pages/Deliveries';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Signin} />
-      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/deliveries" component={Deliveries} isPrivate />
     </Switch>
   );
 }

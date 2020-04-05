b143a022af08f42d5d5f923dc58e952e8fa10a8c
Sandro Santos <sandro.dev@gmail.com>
2020-04-05 10:36:49 -0300

    [fixed] react-toastify CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
 
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-
 import './config/ReactotronConfig';
-
-import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 import history from './services/history';
 

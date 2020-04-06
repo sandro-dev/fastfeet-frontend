c07f26867e2fe39902507fda8cb1b5625a3829f7
Sandro Santos <sandro.dev@gmail.com>
2020-04-06 15:56:23 -0300

    Deliveries Page

--- src/pages/Deliveries/index.js
+++ src/pages/Deliveries/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import SearchInput from '~/components/Input/SearchInput';
+import Grid from '~/components/Grid';
+import Badge from '~/components/Badge';
 
-export default function Dashboard() {
+export default function Deliveries() {
+  const items = [
+    [
+      '#03',
+      'Frederic Chopin',
+      'Tom Hamson',
+      'Salvador',
+      'Bahia',
+      <Badge status="Entregue" color="#2CA42B" bgcolor="#DFF0DF"  />,
+      '...',
+    ],
+    [
+      '#04',
+      'Friederic Nietsche',
+      'Uber eats',
+      'Salvador',
+      'Bahia',
+      <Badge status="Pendente" color="#C1BC35" bgcolor="#F0F0DF"  />,
+      '...',
+    ],
+    [
+      '#05',
+      'Arthur Schoppenhauer dos Santos',
+      'iFood',
+      'Salvador',
+      'Bahia',
+      <Badge status="Retirada" color="#4D85EE" bgcolor="#BAD2FF"  />,
+      '...',
+    ],
+    [
+      '#06',
+      'Mário Sérgio Cortella',
+      'Amazon Prime',
+      'Santa Catarina',
+      'Florianópolis',
+      <Badge status="Cancelada" color="#DE3B3B" bgcolor="#FAB0B0"  />,
+      '...',
+    ],
+  ];
+
   return (
     <>
       <h1>Gerenciando encomendas</h1>
-      <input name="search" placeholder="Buscar por encomendas" />
+      <SearchInput name="search" placeholder="Buscar por encomendas" />
+      <Grid
+        titles={[
+          'ID',
+          'Destinatário',
+          'Entregador',
+          'Cidade',
+          'Estado',
+          'Status',
+          'Ações',
+        ]}
+        itemsArray={items}
+      />
     </>
   );
 }

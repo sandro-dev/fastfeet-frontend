b7059d0460feb579001c35da94c3e7d098d01579
Sandro Santos <sandro.dev@gmail.com>
2020-04-08 08:58:16 -0300

    load data from API and includes Pagination and IconName components, and search with delayed query

--- src/pages/Deliveries/index.js
+++ src/pages/Deliveries/index.js
@@ -1,53 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { debounce } from 'lodash';
 
 import SearchInput from '~/components/Input/SearchInput';
 import Grid from '~/components/Grid';
 import Badge from '~/components/Badge';
+import IconName from '~/components/IconName';
+import Pagination from '~/components/Pagination';
+
+import api from '~/services/api';
 
 export default function Deliveries() {
-  const items = [
-    [
-      '#03',
-      'Frederic Chopin',
-      'Tom Hamson',
-      'Salvador',
-      'Bahia',
-      <Badge status="Entregue" color="#2CA42B" bgcolor="#DFF0DF"  />,
-      '...',
-    ],
-    [
-      '#04',
-      'Friederic Nietsche',
-      'Uber eats',
-      'Salvador',
-      'Bahia',
-      <Badge status="Pendente" color="#C1BC35" bgcolor="#F0F0DF"  />,
-      '...',
-    ],
-    [
-      '#05',
-      'Arthur Schoppenhauer dos Santos',
-      'iFood',
-      'Salvador',
-      'Bahia',
-      <Badge status="Retirada" color="#4D85EE" bgcolor="#BAD2FF"  />,
-      '...',
-    ],
-    [
-      '#06',
-      'Mário Sérgio Cortella',
-      'Amazon Prime',
-      'Santa Catarina',
-      'Florianópolis',
-      <Badge status="Cancelada" color="#DE3B3B" bgcolor="#FAB0B0"  />,
-      '...',
-    ],
-  ];
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalpages, setTotalpages] = useState();
+  const [totalresults, setTotalresults] = useState();
+  const perPage = 10;
+
+  const [loading, setLoading] = useState(true);
+  const [product, setProduct] = useState('');
+  const delayedQuery = useRef(debounce((e) => setProduct(e), 500)).current;
+
+  useEffect(() => {
+    async function loadDeliveries() {
+      const response = await api.get(`deliveries`, {
+        params: { page, perPage, q: product },
+      });
+
+      const data = response.data.results.map((delivery) => [
+        `#${delivery.id}`,
+        delivery.recipient.name,
+        <IconName fullname={delivery.deliveryman.name} />,
+        delivery.recipient.city,
+        delivery.recipient.state,
+        <Badge status="Retirada" color="#4D85EE" bgcolor="#BAD2FF" />,
+        '...',
+      ]);
+
+      setItems(data);
+      setTotalpages(response.data.total_page);
+      setTotalresults(response.data.total_results);
+      setLoading(false);
+    }
+
+    loadDeliveries();
+  }, [page, totalpages, product]);
 
   return (
     <>
       <h1>Gerenciando encomendas</h1>
-      <SearchInput name="search" placeholder="Buscar por encomendas" />
+      <SearchInput
+        name="search"
+        placeholder="Buscar por encomendas"
+        onChange={(e) => delayedQuery(e.target.value)}
+      />
       <Grid
         titles={[
           'ID',
@@ -59,6 +64,13 @@ export default function Deliveries() {
           'Ações',
         ]}
         itemsArray={items}
+        template="0.5fr 1.5fr 2.5fr 1fr 1fr 1fr 1fr"
+      />
+
+      <Pagination
+        page={page}
+        totalpages={totalpages}
+        handlePage={(pg) => setPage(pg)}
       />
     </>
   );

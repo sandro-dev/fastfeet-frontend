5d5f67469d79c42ea7b0d139c876a59d6ec332f4
Sandro Santos <sandro.dev@gmail.com>
2020-04-11 18:05:18 -0300

    HeaderContent component

---
+++ src/components/HeaderContent/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  aside {
+    display: flex;
+    margin-right: 10%;
+  }
+`;

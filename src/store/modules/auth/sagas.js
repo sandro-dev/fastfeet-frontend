3eb58dfc5584d754c97450af7a926925bba01005
Sandro Santos <sandro.dev@gmail.com>
2020-04-12 11:48:09 -0300

    [fix] redirect after signin

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -19,7 +19,7 @@ export function* signIn({ payload }) {
     api.defaults.headers.Authorization = `Bearer ${token}`;
 
     yield put(signInSuccess(token, user));
-    history.push('/dashboard');
+    history.push('/deliveries');
   } catch (error) {
     console.tron.log('error SAGA', error);
     toast.error('Falha na autenticação, verifique seus dados!');

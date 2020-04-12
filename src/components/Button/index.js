4622e617fd68153d83798cfbd6efa63d67079409
Sandro Santos <sandro.dev@gmail.com>
2020-04-12 19:44:42 -0300

    Button component update

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,11 +3,11 @@ import { MdAdd, MdCheck, MdKeyboardArrowLeft } from 'react-icons/md';
 import history from '~/services/history';
 import { Btn } from './styles';
 
-export default function Button({ type, url }) {
+export default function Button({ type, url, ...rest }) {
   switch (type) {
     case 'add':
       return (
-        <Btn bgcolor="#7d40e7" onClick={() => history.push(url)}>
+        <Btn bgcolor="#7d40e7" onClick={() => history.push(url)} {...rest}>
           <MdAdd color="#fff" size={26} />
           <span>CADASTRAR</span>
         </Btn>
@@ -15,7 +15,7 @@ export default function Button({ type, url }) {
 
     case 'save':
       return (
-        <Btn bgcolor="#7d40e7" onClick={() => history.push(url)}>
+        <Btn bgcolor="#7d40e7" {...rest}>
           <MdCheck color="#fff" size={26} />
           <span>SALVAR</span>
         </Btn>

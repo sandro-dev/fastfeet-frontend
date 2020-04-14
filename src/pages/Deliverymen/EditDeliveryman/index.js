c75773846b0de2408e64f3e18fc039fbb2ca8318
Sandro Santos <sandro.dev@gmail.com>
2020-04-14 19:33:02 -0300

    EditDeliveryman page

--- src/pages/Deliverymen/EditDeliveryman/index.js
+++ src/pages/Deliverymen/EditDeliveryman/index.js
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { Form } from '@unform/web';
+import api from '~/services/api';
+import history from '~/services/history';
 
-export default function EditDeliveryman(deliveryman) {
+import HeaderContent from '~/components/HeaderContent';
+import Button from '~/components/Button';
+import ContentForm from '~/components/ContentForm';
+import { UnInput, InputGroup, ImageInput } from '~/components/Input';
+
+export default function InsertDeliveryman(deliveryman) {
   const { id } = deliveryman.match.params;
+  const [preview, setPreview] = useState('');
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await api.get(`deliverymen/${id}`);
+      if (response) {
+        console.log('data -->', response.data);
+        // const avatar = response.data.avatar.url;
+        setPreview(response.data.avatar.url);
+        formRef.current.setData(response.data);
+        // formRef.current.setFieldValue('avatar_id', { id: 105, ...avatar });
+      }
+    }
+    loadData();
+  }, [id]);
+
+  async function handleSubmit(data) {
+    try {
+      const response = await api.post('deliverymen', data);
+      if (response.data.id) {
+        toast.success('Entregador cadastrado com sucesso');
+        history.push('/deliverymen');
+      }
+    } catch (err) {
+      toast.error('Erro ao inserir entregador');
+    }
+  }
 
   return (
     <>
-      <h1>Edit Deliveryman #{id}</h1>
+      <HeaderContent>
+        <div>
+          <h1>Cadastro de entregadores</h1>
+        </div>
+        <aside>
+          <Button type="back" url="/deliverymen" />
+          <Button type="save" url="/deliverymen" form="deliverymen-form" />
+        </aside>
+      </HeaderContent>
+
+      <ContentForm>
+        <Form ref={formRef} id="deliverymen-form" onSubmit={handleSubmit}>
+          <InputGroup>
+            <ImageInput name="avatar_id" imagePreview={preview} />
+          </InputGroup>
+          <InputGroup>
+            <div className="input-block">
+              <UnInput
+                type="text"
+                name="name"
+                label="Nome"
+                placeholder="Nome do entregador"
+              />
+            </div>
+          </InputGroup>
+
+          <InputGroup>
+            <div className="input-block">
+              <UnInput
+                type="email"
+                name="email"
+                label="Email"
+                placeholder="seu@email.com"
+              />
+            </div>
+          </InputGroup>
+        </Form>
+      </ContentForm>
     </>
   );
 }

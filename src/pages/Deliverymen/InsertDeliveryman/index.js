24f9f5e665b6e0f69fef13a9361d476595bc6855
Sandro Santos <sandro.dev@gmail.com>
2020-04-14 19:33:31 -0300

    InsertDelivery page validation of errors

--- src/pages/Deliverymen/InsertDeliveryman/index.js
+++ src/pages/Deliverymen/InsertDeliveryman/index.js
@@ -1,11 +1,13 @@
 import React, { useRef } from 'react';
+import * as Yup from 'yup';
 import { toast } from 'react-toastify';
+import { Form } from '@unform/web';
 import api from '~/services/api';
 import history from '~/services/history';
 
 import HeaderContent from '~/components/HeaderContent';
 import Button from '~/components/Button';
-import Unform from '~/components/Unform';
+import ContentForm from '~/components/ContentForm';
 import { UnInput, InputGroup, ImageInput } from '~/components/Input';
 
 export default function InsertDeliveryman() {
@@ -13,6 +15,14 @@ export default function InsertDeliveryman() {
 
   async function handleSubmit(data) {
     try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('O nome é obrigatório'),
+        email: Yup.string().required('O email é obrigatório'),
+        avatar_id: Yup.number().notRequired(),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+
       const response = await api.post('deliverymen', data);
       if (response.data.id) {
         toast.success('Entregador cadastrado com sucesso');
@@ -20,6 +30,15 @@ export default function InsertDeliveryman() {
       }
     } catch (err) {
       toast.error('Erro ao inserir entregador');
+
+      const validationErrors = {};
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path] = error.message;
+        });
+        formRef.current.setErrors(validationErrors);
+        console.log(validationErrors);
+      }
     }
   }
 
@@ -35,32 +54,34 @@ export default function InsertDeliveryman() {
         </aside>
       </HeaderContent>
 
-      <Unform id="deliverymen-form" ref={formRef} onSubmit={handleSubmit}>
-        <InputGroup>
-          <ImageInput name="avatar_id" />
-        </InputGroup>
-        <InputGroup>
-          <div className="input-block">
-            <UnInput
-              type="text"
-              name="name"
-              label="Nome"
-              placeholder="Nome do entregador"
-            />
-          </div>
-        </InputGroup>
+      <ContentForm>
+        <Form ref={formRef} id="deliverymen-form" onSubmit={handleSubmit}>
+          <InputGroup>
+            <ImageInput name="avatar_id" />
+          </InputGroup>
+          <InputGroup>
+            <div className="input-block">
+              <UnInput
+                type="text"
+                name="name"
+                label="Nome"
+                placeholder="Nome do entregador"
+              />
+            </div>
+          </InputGroup>
 
-        <InputGroup>
-          <div className="input-block">
-            <UnInput
-              type="email"
-              name="email"
-              label="Email"
-              placeholder="seu@email.com"
-            />
-          </div>
-        </InputGroup>
-      </Unform>
+          <InputGroup>
+            <div className="input-block">
+              <UnInput
+                type="email"
+                name="email"
+                label="Email"
+                placeholder="seu@email.com"
+              />
+            </div>
+          </InputGroup>
+        </Form>
+      </ContentForm>
     </>
   );
 }
